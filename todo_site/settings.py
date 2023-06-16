7689829ce7cabed8f0b3fe5a320c75fa7e081de1
Muthupandi <iamkmuthupandi@gmail.com>
2023-06-16 11:35:29 +0530

    second commit

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@7$#)46w&res7l^i69m)+$o*(1j)9@w+zswcf_(*=fl35qpev%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
